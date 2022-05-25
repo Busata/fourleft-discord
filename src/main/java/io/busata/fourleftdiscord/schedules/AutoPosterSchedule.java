179b8830dda31b1d973b8ffecf00c38d2f3bc6f1
busata <driesdesmet@gmail.com>
2022-05-25 11:26:57 +0200

    post previous

--- src/main/java/io/busata/fourleftdiscord/schedules/AutoPosterSchedule.java
+++ src/main/java/io/busata/fourleftdiscord/schedules/AutoPosterSchedule.java
@@ -31,7 +31,7 @@ public class AutoPosterSchedule {
         log.info("Community events check complete.");
     }
 
-    @Scheduled(cron = "0 26 11 * * *", zone="Europe/Brussels")
+    @Scheduled(cron = "0  58 9 * * *", zone="Europe/Brussels")
     public void postAutomatedDailyClubResults() {
         log.info("Posting Daily results");
         autoPosterAutomatedClubService.postResults();
