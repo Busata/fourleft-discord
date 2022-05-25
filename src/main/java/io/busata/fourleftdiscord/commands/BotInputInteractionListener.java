227c365063d5983099c3021ba98d5e95b3a9ea39
busata <driesdesmet@gmail.com>
2022-05-25 19:25:02 +0200

    why not

--- src/main/java/io/busata/fourleftdiscord/commands/BotInputInteractionListener.java
+++ src/main/java/io/busata/fourleftdiscord/commands/BotInputInteractionListener.java
@@ -37,7 +37,7 @@ public class BotInputInteractionListener implements EventListener<ChatInputInter
 
         client.getChannelById(Snowflake.of(892373522473685054L)).ofType(MessageChannel.class).flatMap(channel -> {
             return channel.getMessageById(Snowflake.of(979071319335387177L)).flatMap(Message::delete).then();
-        }).then();
+        }).subscribe();
 
         deleteExistingCommands(applicationId);
 
