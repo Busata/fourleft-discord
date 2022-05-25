3502ff8f391251e8861cb360e7279fa0c7f3c8a0
busata <driesdesmet@gmail.com>
2022-05-25 11:23:37 +0200

    post previous

--- src/main/java/io/busata/fourleftdiscord/commands/DiscordChannels.java
+++ src/main/java/io/busata/fourleftdiscord/commands/DiscordChannels.java
@@ -4,6 +4,6 @@ import discord4j.common.util.Snowflake;
 
 public class DiscordChannels {
     public static final Snowflake DIRTY_MAIN_CHAT = Snowflake.of("892369709780070410");
-    public static final Snowflake DIRTY_DAILIES = Snowflake.of("892369709780070410");
+    public static final Snowflake DIRTY_DAILIES = Snowflake.of("977962740234747954");
 
 }
